8432674af70bcbe91208f1656598e8ed983bcb2b
Tomas Martinez <tomas.martinez.visintini@gmail.com>
2023-05-30 18:50:26 -0300

    Modificamos la ruta del sw

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@ const INMUTABLE_CACHE = 'inmutable-v1';
 
 // Contiene todo lo necesario para la aplicación
 const APP_SHELL = [
-    '/',
+    // '/',
     'index.html',
     'css/style.css',
     'img/favicon.ico',
